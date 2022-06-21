0411e74441e6f569e4312d13a46b963f1c5009de
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-06-21 18:14:24 +0100

    started get routes :octocat:

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const app = express();
+
+const farms = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/farms.json`, "utf-8")
+);
+app.get("/", (req, res) => {
+  res.send("Hello from the Bloomfarm Server!");
+});
+
+app.get("/api/v1/farms", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: farms.length,
+    data: {
+      farms,
+    },
+  });
+});
+
+const port = 8000;
+app.listen(port, () => console.log(`App running on port ${port}...`));
