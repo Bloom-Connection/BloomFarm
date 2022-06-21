29bafcff51d4de07be547d52e92c243781ff7954
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-06-21 22:12:38 +0100

    setup routes for reading and creating a new farm :octocat:

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+
 const farms = JSON.parse(
   fs.readFileSync(`${__dirname}/data/farms.json`, "utf-8")
 );
@@ -9,6 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello from the Bloomfarm Server!");
 });
 
+// Route for the farms
 app.get("/api/v1/farms", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -16,6 +19,24 @@ app.get("/api/v1/farms", (req, res) => {
     data: {
       farms,
     },
+  });
+});
+
+// Creating a new farm route
+app.post("/api/v1/farms", (req, res) => {
+  const newId = farms[farms.length - 1].id + 1;
+  const newFarm = {
+    id: newId,
+    ...req.body,
+  };
+  farms.push(newFarm);
+  fs.writeFile(`${__dirname}/data/farms.json`, JSON.stringify(farms), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        farms: newFarm,
+      },
+    });
   });
 });
 
