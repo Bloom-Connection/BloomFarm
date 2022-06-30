167d93b9959fa65b56aaa793736b7f7ecde6edcc
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-06-30 10:12:24 +0100

    added local file to serve as temp db:octocat:

---
+++ data/import-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Farm = require('./../models/farmModel');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
+//   READ JSON FILE
+const farms = JSON.parse(fs.readFileSync(`${__dirname}/farms.json`, 'utf-8'));
+
+//  IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Farm.create(farms);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Farm.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
