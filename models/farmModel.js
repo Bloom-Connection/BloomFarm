c23bb1068b144064984aa69741c5e235db6993ef
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-06-30 10:15:41 +0100

    added farmModel for farm Schema :octocat:

---
+++ models/farmModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const farmSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A farm must have a name'],
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: [true, 'A farm must have a location'],
+  },
+  description: {
+    type: String,
+    required: [true, 'A farm must have a description'],
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A farm must have an image'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Farm = mongoose.model('Farm', farmSchema);
+
+module.exports = Farm;
