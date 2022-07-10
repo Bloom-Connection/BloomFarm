7046bbd951040165426c1058c4ace34d355f6513
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-07-10 22:40:49 +0100

    updated farmController :octocat:

--- controllers/farmController.js
+++ controllers/farmController.js
@@ -1,39 +1,21 @@
 const Farm = require('../models/farmModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopFarms = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  next();
+};
 
 exports.getAllFarms = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    console.log(queryStr);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    const query = Farm.find(JSON.parse(queryStr));
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query.sort(sortBy);
-    } else {
-      query.sort('-createdAt');
-    }
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query.select(fields);
-    } else {
-      query.select('-__v');
-    }
-    if (req.query.page) {
-      const page = parseInt(req.query.page, 10);
-      const limit = parseInt(req.query.limit, 10);
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
-      query.skip(startIndex).limit(limit);
-    }
-    const farms = await query;
+    // EXECUTE QUERY
+    const features = new APIFeatures(Farm.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const farms = await features.query;
 
     res.status(200).json({
       status: 'success',
@@ -110,6 +92,43 @@ exports.deleteFarm = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getFarmStats = async (req, res) => {
+  try {
+    const stats = await Farm.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$state' },
+          numRating: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      {
+        $match: { avgPrice: { $gt: 500 } },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
     });
   } catch (err) {
     res.status(404).json({
