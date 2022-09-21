e70462935cd04fa728a833eebc7fc17daad96070
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-09-21 20:49:07 +0100

    feat: added query middleware and validators :octocat:

--- models/farmModel.js
+++ models/farmModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const farmSchema = new mongoose.Schema(
   {
@@ -7,6 +8,8 @@ const farmSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A farm must have a name'],
       trim: true,
+      maxlength: [40, 'A farm name cannot be more than 40 characters'],
+      minlength: [10, 'A farm name must have more than 10 characters'],
     },
     slug: String,
     location: {
@@ -35,18 +38,39 @@ const farmSchema = new mongoose.Schema(
 );
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
-tourSchema.pre('save', function (next) {
+farmSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-// tourSchema.post('save', function(doc, next) {
+// farmSchema.post('save', function(doc, next) {
 //   console.log(doc);
 //   next();
 // })
 
-tourSchema.virtual('durationWeeks').get(function () {
+farmSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// QUERY MIDDLEWARE
+farmSchema.pre(/^find/, function (next) {
+  this.find({ secretFarm: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+farmSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+farmSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretFarm: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
 });
 
 const Farm = mongoose.model('Farm', farmSchema);
